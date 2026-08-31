7b1c13bc256153a66351a69387bc9ec406e1b5a4
Panagiotis Vakalis <p.vakalis@outlook.com>
2026-08-31 16:33:04 +0300

    Intial commit

---
+++ Weather-SwiftUI/Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by pani on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
