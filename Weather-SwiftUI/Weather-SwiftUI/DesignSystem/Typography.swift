892ed5b068c8a940465b20afeb671367e95ce2f0
Panagiotis Vakalis <p.vakalis@outlook.com>
2026-09-01 21:50:51 +0300

    Create the Typography and add the custom fonts in it

---
+++ Weather-SwiftUI/Weather-SwiftUI/DesignSystem/Typography.swift
@@ -0,0 +1,15 @@
+//
+//  Typography.swift
+//  Weather-SwiftUI
+//
+//  Created by Panagiotis Vakalis on 1/9/26.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+	static let heroTemperature = Font.system(size: 104.0, weight: .ultraLight)
+	static let heroCompact = Font.system(size: 56.0, weight: .ultraLight)
+	static let tileLabel = Font.system(size: 12.0, weight: .semibold)
+}
